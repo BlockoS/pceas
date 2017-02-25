f47f735b296fe87986e92d04c8b56b1d7e75fdfc
MooZ <cruz.vincent@gmail.com>
2017-02-25 16:55:56 +0100

    Added "generic" memory swap function.

---
+++ include/swap.h
@@ -0,0 +1,11 @@
+#ifndef PCEAS_SWAP_H
+#define PCEAS_SWAP_H
+
+#include <stdint.h>
+
+/**
+ *
+ */
+void swap(uint8_t* a, uint8_t* b, uint32_t size);
+
+#endif /* PCEAS_SWAP_H */
---
+++ src/swap.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "swap.h"
+
+#define SWAP_BUFFER_SIZE 32
+
+/**
+ *
+ */
+void swap(uint8_t* a, uint8_t *b, uint32_t len)
+{
+    uint8_t buffer[SWAP_BUFFER_SIZE];
+    while(len > 0)
+    {
+        uint32_t sz = (len > SWAP_BUFFER_SIZE) ? SWAP_BUFFER_SIZE : len;
+        memcpy(buffer, a, sz);
+        memcpy(a, b, sz);
+        memcpy(b, buffer, sz);
+        len -= sz;
+    }
+}
